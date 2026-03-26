9ecd7cc29a657a74d684bb6d984963fd8fb03b77
glf30 <greyson.frazier@gmail.com>
2026-03-26 16:06:44 -0400

    Add JWT and route protection

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,9 @@
 const User = require("../models/users-model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const createUser = async (userData) => {
   /*
@@ -34,25 +38,34 @@ const createUser = async (userData) => {
 const loginUser = async (userData) => {
   try {
     // verify that username exists in db
-    const user = await User.findOne(
-        { username: userData.username }
-    );
+    const user = await User.findOne({ username: userData.username });
 
-    if(!user){
-        throw "User not found";
+    if (!user) {
+      throw "User not found";
     }
 
     // compare the incoming password with the hashed password in the database
     // db.compare(incomingPassword, hashedPassword)
     // incoming: userData.password
     // hashed: user.password
-    const isCorrectPassword = await bcrypt.compare(userData.password, user.password);
+    const isCorrectPassword = await bcrypt.compare(
+      userData.password,
+      user.password,
+    );
 
-    if(!isCorrectPassword){
-        throw "Passwords do not match"
+    if (!isCorrectPassword) {
+      throw "Passwords do not match";
     }
 
-    return user;
+    // at this point in the code, if an error has not been thrown, we know the user has successfully logged in
+
+    // we can now setup our JWT token for the user
+    // jwt.sign({ payload }, secretKey)
+    // payload - data that we want to store with the token (can be whatever you want, typically for private user information)
+    // secretKey - your app's exclusive key used to sign the token when it's created and verify the token when the user tries to access a protected route (your app's signature) (can generate these online!)
+    const token = jwt.sign({ username: user.name }, process.env.JWT_SECRET_KEY);
+
+    return { username: user.username, token: token };
   } catch (error) {
     throw error;
   }
--- routes/users-router.js
+++ routes/users-router.js
@@ -3,12 +3,14 @@ const { createUser, loginUser } = require("../controllers/users-controller");
 
 const router = express.Router();
 
-router.post("/", async (req,res) => {
+const verifyToken = require("../middleware/authMiddleware")
+
+// run verifyToken function as middleware right before it hits the /profile route
+router.get("/profile", verifyToken, (req,res) => {
     try {
-        const newUser = await createUser(req.body);
         res.json({
             message: "success",
-            payload: newUser
+            payload: "SECURE USER PROFILE INFORMATION!"
         })
     } catch (error) {
         res.status(500).json({
@@ -18,19 +20,35 @@ router.post("/", async (req,res) => {
     }
 })
 
-router.post("/login", async (req,res) => {
-    try {
-        const userLoggedIn = await loginUser(req.body)
-        res.json({
-            message: "success",
-            payload: `${userLoggedIn.username} has logged in successfully!`
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: "failure",
-            payload: error
-        })
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await createUser(req.body);
+    res.json({
+      message: "success",
+      payload: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "failure",
+      payload: error.message,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userLoggedIn = await loginUser(req.body);
+    res.json({
+      message: "success",
+      payload: `${userLoggedIn.username} has logged in successfully`,
+      token: userLoggedIn.token
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "failure",
+      payload: error,
+    });
+  }
+});
 
 module.exports = router;
